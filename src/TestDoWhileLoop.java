4b79f04fabd46790bae9fe32fdd5156dd2797a91
marika.jeziorna <marika.jeziorna@collibra.com>
2019-01-25 13:49:37 +0100

    zadanie-java-10 added two test classes: TestWileLoop, TestDoWhileLoop

---
+++ src/TestDoWhileLoop.java
@@ -0,0 +1,10 @@
+public class TestDoWhileLoop {
+    public static void main(String[] args) {
+        double i = 0;
+        do {
+            System.out.println("Display number: " + i);
+            i += 0.1;
+        }
+        while (i <= 3.1);
+    }
+}
---
+++ src/TestWileLoop.java
@@ -0,0 +1,9 @@
+public class TestWileLoop {
+    public static void main(String[] args) {
+        double i = 0;
+        while (i <= 3.1) {
+            System.out.println("Display number: " + i);
+            i += 0.1;
+        }
+    }
+}
