a1396e44b0b1904590c9e74b3fa01434c31a6a70
marika.jeziorna <marika.jeziorna@collibra.com>
2019-01-28 14:12:44 +0100

    zadanie-java-10 refactor: println switch to printf

--- src/TestDoWhileLoop.java
+++ src/TestDoWhileLoop.java
@@ -2,7 +2,7 @@ public class TestDoWhileLoop {
     public static void main(String[] args) {
         double i = 0;
         do {
-            System.out.println("Display number: " + i);
+            System.out.printf("Display number: %.2f \n", i);
             i += 0.1;
         }
         while (i <= 3.1);
--- src/TestWileLoop.java
+++ src/TestWileLoop.java
@@ -2,7 +2,7 @@ public class TestWileLoop {
     public static void main(String[] args) {
         double i = 0;
         while (i <= 3.1) {
-            System.out.println("Display number: " + i);
+            System.out.printf("Display number: %.2f \n", i);
             i += 0.1;
         }
     }
